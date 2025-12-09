5b5454ca887b4b0d77e36ee90400744cf02a24f6
Felix E <gomezdora1953@gmail.com>
2025-12-09 09:51:45 -0500

    Update EncoderMotor.h

--- src/EncoderMotor.h
+++ src/EncoderMotor.h
@@ -1,1 +1,30 @@
+#ifndef EncoderMotor_h
+#define EncoderMotor_h
 
+#include <Arduino.h>
+
+class EncoderMotor {
+  private:
+    int pinA;
+    int pinB;
+    int pulsesPerRev;
+    volatile long pulseCount;
+    int direction;
+    unsigned long lastTime;
+    float rpm;
+
+    static EncoderMotor* instance; // para ISR estática
+    static void IRAM_ATTR isrHandler();
+
+  public:
+    EncoderMotor(int a, int b, int ppr);
+
+    void begin();
+    void update();
+
+    float getRPM();
+    int getDirection();
+    long getCount();
+};
+
+#endif
